3c231a4788e713b5e285a1f2ad858ff946b9a7b0
zhengjihao <69883552+QdabuliuQ@users.noreply.github.com>
2024-03-06 14:48:43 +0800

    fix: 修复已知bug

--- src/index.ts
+++ src/index.ts
@@ -49,6 +49,8 @@ try {
   // 入口文件路径
   const entryPath = mainConfig.entry.split("/").filter(Boolean).join("\\");
   const fullPath = path.join(rootPath, entryPath)
+  console.log(fullPath);
+
 
   // 判断入口文件夹是否存在
   if (fs.existsSync(fullPath)) {
